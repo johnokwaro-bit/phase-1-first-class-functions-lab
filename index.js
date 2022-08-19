f5f0bc8e2724d73a8a5a4bf7dbac262608f41589
John Okwaro <john.okwaro@moringaschool.com>
2022-08-19 21:02:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers){
+    const firstTwoDrivers =drivers.slice(0, 2);
+    return firstTwoDrivers;
+}
+    
+const returnLastTwoDrivers = function(drivers){
+    const lastTwoDrivers = drivers.slice(2);
+
+    return lastTwoDrivers;
+
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+const createFareMultiplier = function(x){
+    return function(fare){
+    return  x* fare
+    }
+    }
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    let result = returnFirstTwoDrivers(drivers);
+
+    return result;
+}
+
+function selectDifferentDrivers(drivers, returnLastTwoDrivers){
+    let result = returnLastTwoDrivers(drivers);
+
+    return result;
+}
+
